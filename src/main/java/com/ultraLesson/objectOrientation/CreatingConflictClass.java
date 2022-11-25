7f7b0640bc229905194c67be4b820a8ef0604788
Anuj <anuj.vishwakarma@phonepe.com>
2022-11-25 14:13:17 +0530

    Modified

--- src/main/java/com/ultraLesson/objectOrientation/CreatingConflictClass.java
+++ src/main/java/com/ultraLesson/objectOrientation/CreatingConflictClass.java
@@ -1,4 +1,7 @@
 package com.ultraLesson.objectOrientation;
 
 public class CreatingConflictClass {
+    public static void main(String[] args) {
+        System.out.println("I am user B");
+    }
 }
