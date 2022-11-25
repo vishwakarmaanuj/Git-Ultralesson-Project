e9cd3f362319a641f4f9a473ff83a6f8b7e4bcad
Anuj <anuj.vishwakarma@phonepe.com>
2022-11-25 11:19:28 +0530

    Added conflict class

--- src/main/java/com/ultraLesson/objectOrientation/CreatingConflictClass.java
+++ src/main/java/com/ultraLesson/objectOrientation/CreatingConflictClass.java
@@ -1,7 +1,4 @@
 package com.ultraLesson.objectOrientation;
 
 public class CreatingConflictClass {
-    public static void main(String[] args) {
-        System.out.println("I am user A");
-    }
 }
